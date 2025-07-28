91c998383d16b2da0050d0b485913fd584a4d127
adil-jr <adil.jr@outlook.com>
2025-07-28 13:28:01 -0300

    feat: implement JWT authentication and authorization

---
+++ src/main/java/io/github/adil_jr/forumhub_api/controller/AutenticacaoController.java
@@ -0,0 +1,37 @@
+package io.github.adil_jr.forumhub_api.controller;
+
+import io.github.adil_jr.forumhub_api.model.dto.DadosAutenticacao;
+import io.github.adil_jr.forumhub_api.model.dto.DadosTokenJWT;
+import io.github.adil_jr.forumhub_api.model.entity.Usuario;
+import io.github.adil_jr.forumhub_api.security.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        Authentication authentication = manager.authenticate(authenticationToken);
+
+        String tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+}
--- src/main/java/io/github/adil_jr/forumhub_api/repository/UsuarioRepository.java
+++ src/main/java/io/github/adil_jr/forumhub_api/repository/UsuarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    UserDetails findUserByEmail(String email);
+    UserDetails findByEmail(String email);
 }
