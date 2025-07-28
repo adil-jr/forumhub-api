c3ed283e166c11c518df6795dba8909947e29ef8
adil-jr <adil.jr@outlook.com>
2025-07-28 02:33:29 -0300

    feat: initial project setup with dependencies and database configuration

---
+++ src/main/java/io/github/adil_jr/forumhub_api/ForumhubApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.adil_jr.forumhub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumhubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/adil_jr/forumhub_api/ForumhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adil_jr.forumhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
